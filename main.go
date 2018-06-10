44429c7bf2f61beed4ac5e7f175205b7ef959ab1
ParkJunYong <pjy1418@gmail.com>
2018-06-10 11:00:36 +0900

    http example

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
-)
+) 
 
+func main() {
+	// 루트 경로 함수 지정
+	http.HandleFunc("/" ,func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w,"welcome!")
+	})
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w,"about")
+	})
+
+	http.ListenAndServe(":8080",nil)
+}
